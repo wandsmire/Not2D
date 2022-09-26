402ac37a67a132e196b9f8cdcf9f4a9b562e5f79
WandsMire <reza@mirwanda.com>
2022-09-27 03:41:55 +0900

    initial update

---
+++ core/src/com/mirwanda/nottiled/platformer/savegame.java
@@ -0,0 +1,47 @@
+package com.mirwanda.nottiled.platformer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class savegame {
+
+    public savegame(){}
+
+    public List<KV> getVars() {
+        return vars;
+    }
+
+    public void setVars(List<KV> vars) {
+        this.vars = vars;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public String getMapname() {
+        return mapname;
+    }
+
+    public void setMapname(String mapname) {
+        this.mapname = mapname;
+    }
+
+    public List<KV> vars = new ArrayList<KV>();
+    public float x;
+    public float y;
+
+    public String mapname;
+}
